97300942f209c0bd47a66d177ed554efbfbe5725
robertkendall <kendall@wordcircuits.com>
2016-07-15 07:11:42 -0400

    Check references to files outside cwd

---
+++ test/OutsideWcdSpec.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var config = require('../lib/config');
+var parser = require('../lib/parser');
+
+describe('Outside CWD', function() {
+
+	var testWorkingDir = 'test/fixtures/ref-outside-cwd';
+
+	describe('References', function() {
+		var files;
+		var options = {
+			workingDir: testWorkingDir
+		};
+		beforeEach(function(done) {
+			config.forceSet(options);
+			parser.getReferences().then(function(result) {
+				files = result;
+				done();
+			});
+		});
+		it('Should find file outside of CWD', function() {
+			expect(Object.keys(files.referencedFiles)).to.have.length(1);
+			expect(files.referencedFiles).to.have.property(path.join(process.cwd(), 'test/fixtures/outside-cwd/file1.js'));
+		});
+
+	});
+
+});
---
+++ test/fixtures/ref-outside-cwd/referencing-file.js
@@ -0,0 +1,1 @@
+import module1 from '../outside-cwd/file1.js';
